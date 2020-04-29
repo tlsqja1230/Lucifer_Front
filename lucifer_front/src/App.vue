3a8e9eb9cee7ab31c85bb70dbcfc4a1027efe129
shinbum.kang <tlsqja1230@naver.com>
2020-04-29 23:31:47 +0900

    기본 layout구조, 샘플링 파일 생성

---
+++ lucifer_front/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app id="inspire">
+    <!-- 헤더 bar 컴포넌트 -->
+    <headerBar/>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  import headerBar from '@/components/headerBar.vue'
+  export default {
+    components:{
+      headerBar
+    },
+    props: {
+      source: String,
+    },
+    data(){
+      return {
+        dialog: false,
+      }
+    }
+  }
+</script>
---
+++ lucifer_front/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    sampling 파일
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  },
+  props: {
+  },
+  watch: {
+  },
+  computed: {
+  },
+  data(){
+    return {
+    }
+  },
+  beforeRouteEnter (to, from, next) {
+    next()
+  },
+  created() {
+  },
+  mounted() {
+  },
+  updated() {
+  },
+  methods: {
+  },
+}
+</script>
