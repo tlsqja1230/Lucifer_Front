278ac3cbe2070f174f79c184289670669b0e8602
shinbum.kang <tlsqja1230@naver.com>
2020-05-01 16:30:18 +0900

    vuex 추가

--- lucifer_front/src/main.js
+++ lucifer_front/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 import axios from 'axios';
+import store from './store'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
@@ -10,5 +11,6 @@ Vue.prototype.$http = axios
 new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
