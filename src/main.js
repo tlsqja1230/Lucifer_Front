1de29f794328956d7e6c68fd0121352c02e858f5
shinbum.kang <tlsqja1230@naver.com>
2020-05-05 03:57:09 +0900

    socket 통신 추가

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import axios from 'axios';
 import store from './store'
+import webstomp from 'webstomp-client'
+import sockjs from 'sockjs-client'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
+Vue.prototype.$webstomp = webstomp
+Vue.prototype.$sockjs = sockjs
 
 new Vue({
   router,
--- src/views/Guide.vue
+++ src/views/Guide.vue
@@ -65,6 +65,15 @@
           <Modal @openModal="openModal" :isOpen="isOpen" :title="modalTitle" :contents="modalContents"></Modal>
         </v-flex>
         <v-flex lg3 sm6 xs12>
+          <h1>socket</h1>
+          <v-text-field
+            flat
+            label="serverURL"
+            class="hidden-sm-and-down"
+            v-model="serverURL"
+          />
+          <v-btn small color="primary" @click="socketConnect()">connect socket</v-btn>
+          <v-textarea :value="recvList.length > 0? JSON.stringify(recvList) : ''" readonly></v-textarea>
         </v-flex>
       </v-layout>
     </v-container>
@@ -89,15 +98,24 @@ export default {
   },
   data(){
     return {
+      //todo
       todoList:[],
       todoVal: '',
+
+      //api
       title: '',
       author: '',
       content: '',
       apiResult: {},
       isOpen: false,
+
+      //modal
       modalTitle: '',
-      modalContents: ''
+      modalContents: '',
+
+      //socket
+      serverURL: '',
+      recvList: []
     }
   },
   beforeRouteEnter (to, from, next) {
@@ -135,7 +153,32 @@ export default {
     },
     openModal(){
       this.isOpen = !this.isOpen
-    }
+    },
+    socketConnect() {
+      let socket = new this.$sockjs(this.serverURL);
+      this.stompClient = this.$webstomp.over(socket);
+      this.stompClient.connect(
+        {},
+        frame => {
+          // 소켓 연결 성공
+          this.connected = true;
+          console.log('소켓 연결 성공', frame);
+          // 서버의 메시지 전송 endpoint를 구독합니다.
+          // 이런형태를 pub sub 구조라고 합니다.
+          this.stompClient.subscribe("/chart", res => {
+            console.log('구독으로 받은 메시지 입니다.', res.body);
+
+            // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
+            this.recvList.push(JSON.parse(res.body))
+          });
+        },
+        error => {
+          // 소켓 연결 실패
+          console.log('소켓 연결 실패', error);
+          this.connected = false;
+        }
+      );        
+    } 
   },
 }
 </script>
